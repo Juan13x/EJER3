7fc408e07acb9ff0966eb3d44cc95d4735893018
Juan <juang15x@gmail.com>
2022-06-18 19:31:47 -0500

    Version 1.0

--- app/src/main/java/com/juanguicj/ejer3/MainActivity.kt
+++ app/src/main/java/com/juanguicj/ejer3/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.juanguicj.ejer3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.juanguicj.ejer3.databinding.ActivityMainBinding
 import java.util.*
-import java.util.zip.Inflater
 import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
@@ -19,34 +19,38 @@ class MainActivity : AppCompatActivity() {
         val view = mainBinding.root
         setContentView(view)
 
-        with(mainBinding){
+        with(mainBinding) {
             solveButton.setOnClickListener {
                 val currency1 = currency1EditText.text.toString()
 
-                if(currency1.isNullOrEmpty()){
-                    currency1EditText.setHintTextColor(resources.getColor(R.color.main_currency_empty))
+                if (currency1.isEmpty()) {
+                    currency1EditText.setHintTextColor(
+                        ContextCompat.getColor
+                            (applicationContext, R.color.main_currency_empty)
+                    )
                     resultTextView.text = getString(R.string.main_solve_currency_empty)
                     timer.schedule(
                         timerTask {
-                            currency1EditText.setHintTextColor(resources.getColor(R.color
-                                .main_currency_hint))
+                            currency1EditText.setHintTextColor(
+                                ContextCompat.getColor
+                                    (applicationContext, R.color.main_currency_hint)
+                            )
                             resultTextView.text = ""
                         },
                         1000
                     )
 
-                } else{
+                } else {
                     val choiceCurrency1 = choice1Spinner.selectedItem.toString()
                     val choiceCurrency2 = choice2Spinner.selectedItem.toString()
 
                     var valueCurrency1 = currency1.toFloat()
-                    var result = 0
-                    when (choiceCurrency1){
+                    when (choiceCurrency1) {
                         "USD" -> valueCurrency1 *= usd
                         "EUR" -> valueCurrency1 *= eur
                     }
 
-                    when (choiceCurrency2){
+                    when (choiceCurrency2) {
                         "USD" -> valueCurrency1 /= usd
                         "EUR" -> valueCurrency1 /= eur
                     }
